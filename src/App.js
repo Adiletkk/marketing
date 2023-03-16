b7d65c2f0eb4aaf700e83b7a71cb072409284327
Adiletkk <adilet_esirkepov02@mail.ru>
2023-03-16 19:58:53 +0500

    create more slider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import DevCommentsBlock from './components/devComemmentsBLock/DevCommentsBlock';
-import Faqs from './components/faqs/Faqs';
-import PriceInfo from './components/layout-userLock/PriceInfo';
-import Posts from './components/posts/Posts';
-import TeamInfo from './components/userBlock/TeamInfo';
 import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="container">
       <HomePage/>
-      <TeamInfo/>
-      <PriceInfo/>
-      <Faqs/>
-      <DevCommentsBlock/>
-      <Posts/>
     </div>
   );
 }
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import BankCard from '../../components/BankCard/BankCard'
 import Block from '../../components/Block/Block'
 import Card from '../../components/Card/Card'
+import DevCommentsBlock from '../../components/devComemmentsBLock/DevCommentsBlock'
+import Faqs from '../../components/faqs/Faqs'
+import PriceInfo from '../../components/layout-userLock/PriceInfo'
 import MoreCard from '../../components/MoreCard/MoreCard'
+import Posts from '../../components/posts/Posts'
+import TeamInfo from '../../components/userBlock/TeamInfo'
 import Header from '../HeaderPage/Header'
 import ImagePage from '../ImagePage/ImagePage'
 import Main from '../MainPage/Main'
 import css from './HomePage.module.css'
+
+
+
 
 function HomePage() {
   return (
@@ -18,6 +26,11 @@ function HomePage() {
         <Card/>
         <BankCard/>
         <MoreCard/>
+        <TeamInfo/>
+      <PriceInfo/>
+      <Faqs/>
+      <DevCommentsBlock/>
+      <Posts/>
     </div>
   )
 }
