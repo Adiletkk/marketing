8f2e0cbba96bbdb64d6513632b659f983be026b1
Adiletkk <adilet_esirkepov02@mail.ru>
2023-03-17 14:37:18 +0500

    card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import DevCommentsBlock from './components/devComemmentsBLock/DevCommentsBlock';
-import Faqs from './components/faqs/Faqs';
-import Footer from './components/footer/Footer';
-import Form from './components/form/form';
-import PriceInfo from './components/layout-userLock/PriceInfo';
-import Posts from './components/posts/Posts';
-import TeamInfo from './components/userBlock/TeamInfo';
+
 import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="container">
       <HomePage/>
-      <TeamInfo/>
-      <PriceInfo/>
-      <Faqs/>
-      <DevCommentsBlock/>
-      <Posts/>
-      <Form/>
     </div>
   );
 }
