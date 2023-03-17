d2e67c3d17470fa3d91fbd095ede114ced3c3995
daniihehyhe <115088264+daniihehyhe@users.noreply.github.com>
2023-03-17 14:46:28 +0600

    slider

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import BankCard from '../../components/BankCard/BankCard'
 import Block from '../../components/Block/Block'
 import Card from '../../components/Card/Card'
+import DevCommentsBlock from '../../components/devComemmentsBLock/DevCommentsBlock'
+import Faqs from '../../components/faqs/Faqs'
+import PriceInfo from '../../components/layout-userLock/PriceInfo'
 import MoreCard from '../../components/MoreCard/MoreCard'
+import Posts from '../../components/posts/Posts'
+import TeamInfo from '../../components/userBlock/TeamInfo'
 import Header from '../HeaderPage/Header'
 import ImagePage from '../ImagePage/ImagePage'
 import Main from '../MainPage/Main'
@@ -11,13 +16,19 @@ import css from './HomePage.module.css'
 function HomePage() {
   return (
     <div className={css.wrapper}>
-        <Header/>
-        <Main/>
-        <ImagePage/>
-        <Block/>
-        <Card/>
-        <BankCard/>
-        <MoreCard/>
+      <Header />
+      <Main />
+      <ImagePage />
+      <Block />
+      <Card />
+      <BankCard />
+      <MoreCard />
+
+      <TeamInfo />
+      <PriceInfo />
+      <Faqs />
+      <DevCommentsBlock />
+      <Posts />
     </div>
   )
 }
